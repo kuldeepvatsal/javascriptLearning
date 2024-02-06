fa1a6b5b28fbf6ab451109f729d083799ce35715
kuldeep Singh <54655091+kuldeepvatsal@users.noreply.github.com>
2024-02-06 01:51:54 +0000

    addedVariablefile

---
+++ jsBasic/01_variable.js
@@ -0,0 +1,9 @@
+const accountId=234353
+let accountEmail="kuldeep@gmail.com"
+var accountPassword="4321"
+accountCity ="Noida"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+/*
+note to use Var because issue of  functional block and  scope issue
+*/
