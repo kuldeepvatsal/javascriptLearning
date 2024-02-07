c41e1a1e96574d699673295b64d9a52327dd2afc
kuldeep Singh <54655091+kuldeepvatsal@users.noreply.github.com>
2024-02-07 02:17:37 +0000

    addedconversion

---
+++ jsBasic/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33io"
+// let convertedScore=Number(score)
+// console.log(convertedScore)
+// console.log(typeof(convertedScore))
+// NaN types is number
+// let isLoggin=1
+// console.log(isLoggin)
+// let convertToBoolean=Boolean(isLoggin)
+// console.log(convertToBoolean)
+// console.log(typeof convertToBoolean)
+
+// let keyword=undefined//NaN;//null
+// console.log(keyword)
+// let convertToNumber=Number(keyword)
+// console.log(convertToNumber)
+// console.log(typeof keyword)
+
+//"33=>33
+//33abc=>NaN
