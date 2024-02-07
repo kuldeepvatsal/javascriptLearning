f249924feef8ef239fb7a87c649366dafa3599ca
kuldeep Singh <54655091+kuldeepvatsal@users.noreply.github.com>
2024-02-07 01:58:07 +0000

    added DataTypes

---
+++ jsBasic/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js as newer version
+//alert(3+3) //we are using nodejs,not browser
+console.log(3+3)
+console.log("Kuldeep")
+let name="kuldeep";
+let age =40
+let isLoggedin=false
+//datatypes
+//number
+// string
+//boolean
+//null=>standalone type is object
+//undefined type is undefined
+//symbol =>unique 
+
+
+//object
+console.log(typeof(Symbol))
