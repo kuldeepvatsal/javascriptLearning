94b38f515c83a9751bf63943e4c49a5e2e4cbe75
kuldeep Singh <54655091+kuldeepvatsal@users.noreply.github.com>
2024-02-16 02:47:16 +0000

    added files

---
+++ jsBasic/MemoryTypes.js
@@ -0,0 +1,11 @@
+//
+///Stack- Primitive TYpes
+          //String,number,boolean,
+          
+//Heap non primitice-Array,{}
+
+let userOne={name:"kuldeep",upi:"user@ybl"}
+let userTwo=userOne
+userTwo.upi="test@ybl"
+console.log(userTwo.upi)
+console.log(userOne.upi)
---
+++ jsBasic/stringstest.js
@@ -0,0 +1,3 @@
+const nametexxt="kuldeep"
+const repo=50
+console.log(nametexxt+ "reop")
